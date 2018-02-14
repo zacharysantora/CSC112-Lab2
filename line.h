fa5e54ce5823eba634df5a76dfd80bb8db787a01
Robert Lowe <robert.lowe@maryvillecollege.edu>
2018-02-14 16:18:05 -0500

    Created class definition of the line class.

---
+++ line.h
@@ -0,0 +1,30 @@
+//Line class, draw a line between two points!
+#ifndef LINE_H
+#define LINE_H
+#include "shape.h"
+
+class Line : public Shape
+{
+public:
+    Line(); //public constructor
+
+    //required widget functions
+    virtual void display();
+    virtual void handleEvent(Event *e);
+
+    //shape specific functions
+    virtual bool isComplete();
+    virtual void addPoint(int _x, int _y);
+
+    //return the ending x and y
+    virtual int ex();
+    virtual int ey();
+private:
+    bool _complete;
+    int _ex, _ey;
+};
+
+#endif
+
+
+    
