766030a243e94b5f07afb6983cf778e83661834f
Sarah Woodward <sarah.woodward@my.maryvillecollege.edu>
2018-02-15 21:15:55 +0000

    rectangle.h is done

--- rectangle.h
+++ rectangle.h
@@ -1,26 +1,26 @@
-<<<<<<< HEAD
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+#include "shape.h"
+
 class Rectangle : public Shape
 {
-public:
+    public:
+    //default constructor
+        Rectangle(); //public constuctor
 
-Rectangle(); //public constuctor
-
-virtual void addPoint(int_x, int_y);
-
-
-virtual void display();
-virtual void handleEvent(Event *e);
-
-virtual bool isComplete();
-
-private:
+        virtual void addPoint(int_x, int_y);
 
 
-int x1,y1;
-int x2,y2
-int pointCount;
-};
-#endif
-=======
-//Rectangle class, draw a rectangle based on 2 points
->>>>>>> 4fa78b5c061dd2ae96985f5c92fc3dc023fd5e2f
+        virtual void display();
+        virtual void handleEvent(Event *e);
+
+        virtual bool isComplete();
+
+    private:
+
+        int x1,y1;
+        int x2,y2;
+        int pointCount;
+    };
+    #endif
+
