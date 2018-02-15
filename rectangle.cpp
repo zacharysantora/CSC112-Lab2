efcf8a3c609bfdbf689aac3155beefa8659733ab
Sarah Woodward <sarah.woodward@my.maryvillecollege.edu>
2018-02-15 20:18:15 +0000

    notes only

---
+++ rectangle.cpp
@@ -0,0 +1,16 @@
+#include iostream
+#include "point.h"
+#include "rectangle.h"
+
+
+using namespace std;
+
+//when user presses r = x1,y1 coordinate
+//when user presses r again = x2,y2 coordinate
+
+//line function (x1,y1) to (x1,y2)
+//line function (x1,y2) to (x2,y2) 
+//line function (x2,y2) to (x2,y1)
+//line function (x2,y2) to (x1,y1)
+
+//return 0;
