8a2aadc162a1207c1d2a80f9d663a9d290b3fb99
Sarah Woodward <sarah.woodward@my.maryvillecollege.edu>
2018-02-15 21:32:08 +0000

    done

--- rectangle.cpp
+++ rectangle.cpp
@@ -37,18 +37,4 @@ void Rectangle::display()
 void 
 Rectangle::handleEvent(Event *e)
 {
-    //do nothing
-}
-
-//when user presses r = x1,y1 coordinate
-//when user presses r again = x2,y2 coordinate
-
-
-
-//line function (x1,y1) to (x1,y2)
-//line function (x1,y2) to (x2,y2) 
-//line function (x2,y2) to (x2,y1)
-//line function (x2,y2) to (x1,y1)
-
-//return 0;
-
+    
