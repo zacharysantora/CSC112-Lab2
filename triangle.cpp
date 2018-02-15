3e7ce3f69eb40374546259c7e13b3dce4a03fd6f
Caleb Underdown <caleb.underdown@my.maryvillecollege.edu>
2018-02-15 22:59:05 +0000

    changes

--- triangle.cpp
+++ triangle.cpp
@@ -9,6 +9,8 @@ triangle::triangle(){
     //where does it start
     x1 = 0;
     y1 = 0;
+    x3 = 0;
+    x4 = 0;
     pointCount = 0;
 }
 
@@ -17,17 +19,22 @@ void triangle::display(){
     if(not iscomplete()){
         return;
     }
-     Line 8,9;
-    8.addpoint(x1,y1);
-    8.display();
+    c=2*(y2-y1);
+    x3=x2+c;
+    x4=x2-c;
+     Line a,b,c;
+    a.addpoint(x1,y1);
+    a.display();
     
-    9.addpoint(x2,y2);
-    9.display();
+    b.addpoint(x2,y2);
+    b.display();
     
-    9.addpoint(x3,y2);
-    9.apppoint(-x3,y2)
-    9.display();
-   
+    b.addpoint(x3,y2);
+    b.apppoint(x4,y2);
+    b.display();
+    
+    c.addpoint(x1,y1);
+    c.display();
 
 }
 void triangle::handleEvent(Event *e){
@@ -47,15 +54,17 @@ void triangle::addpoint(int _x, int _y){
         y1 =(_y);
         cout << x1 << "*" << y1 << endl;
     } else if (pointCount == 1) {
+        if (x1!=x2){
+            cout << "pick another point" << endl;
+        }
         x2 = (_x);
         y2 = (_y);
         cout << x2 << "*" << y2 << endl;
-    } else if (pointCounter == 2){
         dy = y2 - y1;
-        l = sqrt(2*(pow(dy,2));
+        l = sqrt(2*(pow(dy,2)));
         x3 = .5 * l
         cout << x3 << "*" << y2 << endl;
-        cout << -x3 << "*" << y2 << endl;
-    }
+        cout << x4 << "*" << y2 << endl;
+                 }
     pointCount++;
 }
