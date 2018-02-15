e6b8649c76553fe964556c09a6c740ae5aa2bf42
jordanmw08 <jordan.woodward@my.maryvillecollege.edu>
2018-02-15 21:17:13 +0000

    add.point s lines

--- rectangle.cpp
+++ rectangle.cpp
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-=======
-//makes a rectangle
-
->>>>>>> 4fa78b5c061dd2ae96985f5c92fc3dc023fd5e2f
 #include iostream
 #include "point.h"
 #include "rectangle.h"
@@ -25,16 +20,33 @@ void Rectangle::display()
     l1.addPoint(x2,y2);
     l1.display();
     
-    l2.addPoint(x1,y1)
+    l2.addPoint(x1,y1);
+    l2.addPoint(x2,y2);
+    l2.display();
+    
+    l3.addPoint(x2,y2);
+    l3.addPoint(x2,y1);
+    l3.display();
+    
+    l4.addPoint(x2,y2);
+    l4.addPoint(x1,y1);
+    l4.display();
     
 }
+
+void 
+Rectangle::handleEvent(Event *e)
+{
+    //do nothing
+}
+
 //when user presses r = x1,y1 coordinate
 //when user presses r again = x2,y2 coordinate
 
 
 
 //line function (x1,y1) to (x1,y2)
-//line function (x1,y2) to (x2,y2)
+//line function (x1,y2) to (x2,y2) 
 //line function (x2,y2) to (x2,y1)
 //line function (x2,y2) to (x1,y1)
 
