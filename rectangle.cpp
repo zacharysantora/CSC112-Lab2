0218cf0647340684d55066041e0f2a77664fe543
Sarah Woodward <sarah.woodward@my.maryvillecollege.edu>
2018-02-15 20:25:00 +0000

    notes and points

--- rectangle.cpp
+++ rectangle.cpp
@@ -8,6 +8,12 @@ using namespace std;
 //when user presses r = x1,y1 coordinate
 //when user presses r again = x2,y2 coordinate
 
+//add points
+l1.addPoint(x1,y1);
+l1.addPoint(x1,y2);
+l1.addPoint(x2,y1);
+l1.addPoint(x2,y2);
+
 //line function (x1,y1) to (x1,y2)
 //line function (x1,y2) to (x2,y2) 
 //line function (x2,y2) to (x2,y1)
